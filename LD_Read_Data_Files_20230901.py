c07e5453333d2ea881e491b796d0224e59bcace0
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-01 17:08:10 +0800

    Update LD_Read_Data_Files_20230901.py

--- LD_Read_Data_Files_20230901.py
+++ LD_Read_Data_Files_20230901.py
@@ -90,7 +90,7 @@ with col2:
  
   file_raw='https://github.com/polaryang/Long_Dragon/raw/main/'
   #tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["重大訊息", "公告查詢", "公司基本資料", "董監事持股餘額", "十大股東", "股權分散表", "議事錄","徵求作業日程表"])
-  tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["重大訊息", "公告查詢", "公司基本資料", "董監事持股餘額", "十大股東", "股權分散表", "議事錄"])
+  tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["重大訊息", "公告查詢", "公司基本資料", "董監事持股餘額", "十大股東*", "股權分散表*", "議事錄*"])
   with tab1:
     # 1.	重大訊息
     # 先執行 https://mopsfin.twse.com.tw/opendata/t187ap04_L.csv 每日更新
