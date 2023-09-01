e3b709599ead0ee8f469d476348c3ed2edef7c22
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-02 00:27:08 +0800

    Update LD_Read_Data_Files_20230901.py

--- LD_Read_Data_Files_20230901.py
+++ LD_Read_Data_Files_20230901.py
@@ -197,7 +197,8 @@ with col2:
     df_control['Control_ratio']=df_control['最終控制者個人持股%']+df_control['集團未上市公司持股%']+df_control['集團基金會持股%']+df_control['集團上市公司持股%']+df_control['經理人持股%']+df_control['外部個人持股%']+df_control['外部未上市公司持股%']+df_control['外部基金會持股%']+df_control['外部上市公司持股%']
     df_control_class = df_control.groupby('控制別').sum()
     df_control_results=df_control_class['Control_ratio']
-    st.bar_chart(df_control_results, use_container_width=True,color=['#FF0000','#00FFFF','#FFFF00','#0000FF','#FF00FF'])
+    df.rename(index={'A':'最終控制者', 'B':'經理人', 'C':'集團經理人', 'L':'友好集團', 'X':'外部人'}, inplace=True)
+    st.bar_chart(df_control_results, use_container_width=True)
     st.write('資料截止日期: '+str(collect_date))
     
   with tab6:  
