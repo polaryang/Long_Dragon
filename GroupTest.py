3bf87cd05286c7df07aba1b46b45d8ed99d6e1a9
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-05 14:51:37 +0800

    Update GroupTest.py

--- GroupTest.py
+++ GroupTest.py
@@ -273,7 +273,7 @@ with col2:
   df_control=db_control[db_control['公司']==id]
   nodes.append( Node(id=ID_code, label=ID_name, size=20, color='blue') )  
   st.write(len(df_control))  
-  for i in range(len(df_control)):
+  for i in range(1,5):
       df_control['持股人集團名'][i]
       st.write(df_control['持股人集團名'][i])
       nodes.append( Node(id=df_control['持股人集團名'][i], size=10, color='red') ) 
