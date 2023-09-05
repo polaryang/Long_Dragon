c9250a085f2746e7eb6e544e83f6104554590b32
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-05 19:11:11 +0800

    Update GroupTest.py

--- GroupTest.py
+++ GroupTest.py
@@ -283,11 +283,11 @@ with col2:
       df_control_invested=df_control_invested[df_control_invested['公司']!=id]
       df_control_invested_id=df_control_invested['公司'] #被控制者 投資的 公司代號
       df_control_invested_name=df_control_invested['簡稱'] #被控制者 投資的 公司名稱
-      control_invested_id=pd.Dataframe(pd.unique(pd.Series(df_control_invested_id)))
-      control_invested_name=pd.Dataframe(pd.unique(pd.Series(df_control_invested_name)))
+      #control_invested_id=pd.Dataframe(pd.unique(pd.Series(df_control_invested_id)))
+      #control_invested_name=pd.Dataframe(pd.unique(pd.Series(df_control_invested_name)))
       for j in range(len(control_invested_id)):
           st.write(j)
-          st.write(type(control_invested_id))
+          st.write(type(df_control_invested_id))
           #st.write(str(control_invested_id.iloc[j,0]), control_invested_name.iloc[j,1]) 
           #investor=df_control_investor.iloc[j,5]
           nodes.append( Node(id=control_invested_id, label=control_invested_name, size=15, color='green') )
