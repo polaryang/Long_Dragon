dd36f63b75fde9c208ef24f01b8a1d376d311c11
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-05 11:55:43 +0800

    Update GroupTest.py

--- GroupTest.py
+++ GroupTest.py
@@ -270,26 +270,16 @@ with col1:
 with col2:
   nodes = []
   edges = []
-  nodes.append( Node(id="Spiderman", 
-                   label="Peter Parker", 
-                   size=25, 
-                   shape="circularImage",
-                   image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
-            ) # includes **kwargs
+  nodes.append( Node(id="Spiderman", label="Peter Parker", size=25, shape="circularImage",) ) 
   nodes.append( Node(id="Captain_Marvel", 
                    size=25,
                    shape="circularImage",
                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
             )
-  edges.append( Edge(source="Captain_Marvel", 
-                   label="friend_of", 
-                   target="Spiderman", 
-                   # **kwargs
-                   ) 
-            ) 
+  edges.append( Edge(source="Captain_Marvel", label="friend_of", target="Spiderman", type="CURVE_SMOOTH" ) ) 
 
-  config = Config(width=750,
-                height=950,
+  config = Config(width=750*2,
+                height=950*2,
                 directed=True, 
                 physics=True, 
                 hierarchical=False,
