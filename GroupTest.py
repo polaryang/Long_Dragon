437e2050b605c6bf475ae55c6dc9c540206bf3c7
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-05 20:33:54 +0800

    Update GroupTest.py

--- GroupTest.py
+++ GroupTest.py
@@ -287,7 +287,7 @@ with col2:
       df_control_invested_name=df_control_invested['簡稱'] #被控制者 投資的 公司名稱
       df_control_invested_id=pd.unique(pd.Series(df_control_invested_id))
       df_control_invested_name=pd.unique(pd.Series(df_control_invested_name))
-      for len(df_control_invested_name >0:
+      for len(df_control_invested_name) >0:
           for j in range(len(df_control_invested_name): #len(df_control_invested_name)
               st.write('被投資者')
               st.write(j)
