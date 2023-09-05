a622d55ce61fdf1a74e098ed2cfe28db1c36c2d1
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-05 19:17:50 +0800

    Update GroupTest.py

--- GroupTest.py
+++ GroupTest.py
@@ -287,7 +287,7 @@ with col2:
       #df_control_invested_name=pd.unique(pd.Series(df_control_invested_name))
       for j in range(len(df_control_invested_name)):
           st.write(j)
-          st.write(df_control_invested_name[0])
+          st.write(df_control_invested_name)
           #st.write(str(control_invested_id.iloc[j,0]), control_invested_name.iloc[j,1]) 
           #investor=df_control_investor.iloc[j,5]
           nodes.append( Node(id=control_invested_id, label=control_invested_name, size=15, color='green') )
