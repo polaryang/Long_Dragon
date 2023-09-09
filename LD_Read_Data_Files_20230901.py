70976174ced5d1b5b8a8a8f306af608aafe82928
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-09 20:46:24 +0800

    Update LD_Read_Data_Files_20230901.py

--- LD_Read_Data_Files_20230901.py
+++ LD_Read_Data_Files_20230901.py
@@ -511,9 +511,12 @@ with col2:
   with tab9: 
     #9.	委託書徵求
     st.subheader('委託書徵求(歷年)')
-    df_entrust=db_entrust[db_entrust['證券代號']==id] 
+    db_entrust['證券代號'] = db_entrust['證券代號'].astype(str)
+    db_entrust['股東常會日期'] = db_entrust['股東常會日期'].astype(str)
+    df_entrust=db_entrust[db_entrust['證券代號']==str(id)] 
+    df_entrust=df_entrust.drop(['證券代號'], axis=1)
     st.dataframe(df_entrust, use_container_width=True,hide_index=True)
-    #df_entrust=df_entrust.drop(['證券代號'], axis=1)
+    #
     #df_entrust=df_entrust.sort_values(by='證券代號', ascending=False)
     #st.dataframe(df_entrust, use_container_width=True,hide_index=True)
     #st.write('全部公司委託書徵求')
