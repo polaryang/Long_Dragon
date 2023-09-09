a483191a24bbe26f38052d40eb1a48f54d59e8bb
James Yang <78498381+polaryang@users.noreply.github.com>
2023-09-09 20:55:05 +0800

    Update LD_Read_Data_Files_20230901.py

--- LD_Read_Data_Files_20230901.py
+++ LD_Read_Data_Files_20230901.py
@@ -518,9 +518,9 @@ with col2:
     df_entrust=df_entrust.sort_values(by='股東常會日期', ascending=False)  
     st.dataframe(df_entrust, use_container_width=True,hide_index=True)
     st.write('全部公司委託書徵求')
-    db_entrust=db_entrust.sort_values(by=['證券代號','股東常會日期'], ascending=False) 
-    #seq=range(1,len(db_entrust)+1)
-    #db_entrust.insert(0,"序號",seq,True)  
+    db_entrust=db_entrust.sort_values(by=['證券代號','股東常會日期'], ascending=True) 
+    seq=range(1,len(db_entrust)+1)
+    db_entrust.insert(0,"序號",seq,True)  
     #db_entrust['證券代號'] = db_entrust['證券代號'].astype(str)
     st.dataframe(db_entrust, use_container_width=True,hide_index=True)
       
